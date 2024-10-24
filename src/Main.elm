7f4e4ad267a6149bbc96b5b0bd59365ad12254ec
rangatanga <charles_penwill@hotmail.com>
2024-10-24 18:35:58 +0100

    .

--- src/Main.elm
+++ src/Main.elm
@@ -5,11 +5,16 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Url
-import Element exposing (Element, el, text, row, alignRight, fill, width, rgb255, spacing, centerY, padding)
+import Element exposing (Element, el, text, row, alignRight, fill, width, rgb255, spacing, centerY, padding, rgb, Color)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Platform exposing (Router)
+import Binary exposing (..)
+import Element.Input
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+--import Html.Events exposing (..)
 
 
 -- MAIN
@@ -32,12 +37,12 @@ main =
 
 
 type alias Model =
-  { subdivisions : Subdivisions
-  , instruments : Instruments
-  , availableInstruments : Instruments
+  { subdivisions : List Subdivision
+  , instruments : List Instrument
+  , availableInstruments : List Instrument
+  , blocks : List Block
+  , arrangement : List InstrumentBlocks
   }
-
-type alias Subdivisions = List Subdivision
 
 type alias Subdivision =
   { name : String
@@ -45,13 +50,11 @@ type alias Subdivision =
   , subBeats : Int
   }
 
-type alias Instruments = List Instrument
-
 type alias Instrument = 
-    { name : String
-    , staveLocation : String
-    , stavePosition : Int 
-    }
+  { name : String
+  , staveLocation : String
+  , stavePosition : Int 
+  }
 
 
 {- 
@@ -67,8 +70,19 @@ G4  3   -------------------------------
 F4  2       (bass drum)
 E4  1   -------------------------------
 D4  0       (hi-hat foot)
-
 -}
+
+type alias Block = 
+  { name : String
+  , imageName : String
+  , notePlacement : Bits
+  , subdivision : String
+  }
+
+type alias InstrumentBlocks = 
+  { instrumentName : String
+  , blockNames : List String
+  }
 
 
 -- INIT
@@ -95,6 +109,35 @@ initialModel =
                              , Instrument "Floor Tom" "A4" 4
                              , Instrument "Hi-hat Foot" "D4" 0
                              ]
+    , blocks = [Block "A" "A.png" (Binary.fromIntegers [1,0,0,0]) "4-16"
+               , Block "B" "B.png" (Binary.fromIntegers [0,1,0,0]) "4-16"
+               , Block "C" "B.png" (Binary.fromIntegers [0,0,1,0]) "4-16"
+               , Block "D" "B.png" (Binary.fromIntegers [0,0,0,1]) "4-16"
+               , Block "E" "B.png" (Binary.fromIntegers [1,1,0,0]) "4-16"
+               , Block "F" "B.png" (Binary.fromIntegers [0,1,1,0]) "4-16"
+               , Block "G" "B.png" (Binary.fromIntegers [0,0,1,1]) "4-16"
+               , Block "H" "B.png" (Binary.fromIntegers [1,0,0,1]) "4-16"
+               , Block "I" "B.png" (Binary.fromIntegers [1,0,1,0]) "4-16"
+               , Block "J" "B.png" (Binary.fromIntegers [0,1,0,1]) "4-16"
+               , Block "K" "B.png" (Binary.fromIntegers [1,1,1,0]) "4-16"
+               , Block "L" "B.png" (Binary.fromIntegers [0,1,1,1]) "4-16"
+               , Block "M" "B.png" (Binary.fromIntegers [1,0,1,1]) "4-16"
+               , Block "N" "B.png" (Binary.fromIntegers [1,1,0,1]) "4-16"
+               , Block "O" "B.png" (Binary.fromIntegers [1,1,1,1]) "4-16"
+               , Block "P" "B.png" (Binary.fromIntegers [0,0,0,0]) "4-16"
+               , Block "Q" "B.png" (Binary.fromIntegers [1,0,0]) "3-8"
+               , Block "R" "B.png" (Binary.fromIntegers [0,1,0]) "3-8"
+               , Block "S" "B.png" (Binary.fromIntegers [0,0,1]) "3-8"
+               , Block "T" "B.png" (Binary.fromIntegers [1,1,0]) "3-8"
+               , Block "U" "B.png" (Binary.fromIntegers [0,1,1]) "3-8"
+               , Block "V" "B.png" (Binary.fromIntegers [1,0,1]) "3-8"
+               , Block "W" "B.png" (Binary.fromIntegers [1,1,1]) "3-8"
+               , Block "X" "B.png" (Binary.fromIntegers [0,0,0]) "3-8"
+               ]
+    , arrangement = [ InstrumentBlocks "Hi-Hat" ["A", "A", "A", "A"]
+                    , InstrumentBlocks "Snare" ["P", "A", "P", "A"]
+                    , InstrumentBlocks "Bass Drum" ["A", "P", "A", "P"]
+                    ]               
     }   
 
 
@@ -104,6 +147,7 @@ initialModel =
 type Msg
   = LinkClicked Browser.UrlRequest
   | UrlChanged Url.Url
+  | BlockClickMsg 
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -127,17 +171,27 @@ view : Model -> Browser.Document Msg
 view model =
   { title = "Drum Blocks"
   , body =
-      [  Element.layout [] <| Element.row 
+      [ Element.layout [] <| Element.column 
+                              [] 
+                              ([Element.row 
                                 [] 
-                                [subdivisionDropdown model]
+                                [subdivisionDropdown model] 
+                               ]++(instrumentView model.arrangement))
+      ,svg
+            [ Svg.Attributes.width "100%"
+            , Svg.Attributes.height "100%"
+            , viewBox "0 0 100 95"
+            ]
+            (stave)
       ]
   }
+
 
 subdivisionDropdown : Model -> Element Msg
 subdivisionDropdown model = 
     el  [Font.size 18] 
         (Element.row [] 
-            [ Element.text "Subdivision: "
+            [ Element.el [Element.width (Element.px 150)] (Element.text "Subdivision:")
             , Element.html (select [] (List.map subdivisionOption model.subdivisions))
             ]
         )
@@ -146,6 +200,33 @@ subdivisionDropdown model =
 subdivisionOption : Subdivision -> Html Msg
 subdivisionOption subdiv = 
     Html.option [] [Html.text subdiv.description]
+
+instrumentView : List InstrumentBlocks -> List (Element Msg)
+instrumentView instrumentBlocks = List.map instrumentRow instrumentBlocks
+
+instrumentRow : InstrumentBlocks -> Element Msg
+instrumentRow instrumentBlock = Element.row [Element.height (Element.px 40)] ([Element.el [Element.width (Element.px 150)] (Element.text instrumentBlock.instrumentName)] ++ blockView instrumentBlock.blockNames)
+
+blockView : List String -> List (Element Msg)
+blockView blocks = List.map blockButton blocks
+
+blockButton : String -> Element Msg
+blockButton block = Element.Input.button 
+                                    [ Background.color (Element.rgb255 238 238 238)
+                                    , Element.focused [Background.color (Element.rgb255 238 238 238)]
+                                    , Element.width (Element.px 60)
+                                    , Border.solid
+                                    , Border.color (rgb 0 0 0)
+                                    , Border.width 2
+                                    , Border.shadow {offset = (12.0,12.0), size = 5, blur = 5, color = (rgb 100 100 100)}
+                                    , Border.rounded 5
+                                    , Font.center
+                                    , Font.size 20
+                                    ]
+                                    { onPress = Just BlockClickMsg
+                                    , label = Element.text block
+                                    }
+
 
 {-
 myElement : String -> Element msg
@@ -158,3 +239,21 @@ myElement txt =
         ]
         (Element.text txt)
 -}
+
+stave : List (Svg Msg)
+stave =
+    (clefLines)
+        |> List.map String.fromInt
+        |> List.map
+            (\n ->
+                Svg.path
+                    [ strokeWidth "0.2"
+                    , stroke "black"
+                    , d ("M 5 " ++ n ++ " L 95 " ++ n)
+                    ]
+                    []
+            )
+
+clefLines : List Int
+clefLines =
+    [ 0, 4, 8, 12, 16 ]
